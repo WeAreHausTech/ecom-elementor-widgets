6d042cac49ffec19c54202281e54ac4ca77e1ef5
Mathias Saxenstrand <mathias@haus.se>
2023-09-05 14:06:19 +0200

    v0.0.3 - added product sorting and pagination

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import { VendureApolloProvider } from '@/providers/vendure/VendureApolloProvider
 // Products
 import { ProductList } from '@/components/product-list/ProductList'
 import { Product } from '@/components/product/Product'
+import { ProductSort } from './products-sort/ProductSort'
 
 // Cart
 import { AddToCart } from './add-to-cart/AddToCart'
@@ -14,6 +15,7 @@ export {
   VendureApolloProvider,
   ProductList,
   Product,
+  ProductSort,
   AddToCart,
   CartContent,
   CartRemoveItem,
