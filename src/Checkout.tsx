3655a671d3eda1deb8b5eb78daf6432dfffdc10c
Johanna Bertils <johanna@haus.se>
2023-09-06 16:36:58 +0200

    Some styling

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -9,107 +9,107 @@ export default function Checkout() {
           return !formData ? (
             <div>Loading...</div>
           ) : (
-            <div className="shipping-address bg-white p-4">
-              <form onSubmit={handleSubmit} className="bg-white p-6 rounded-md shadow-md max-w-md">
+            <div className="shipping-address p-4">
+              <form onSubmit={handleSubmit} className="bg-white p-6 max-w-md">
                 <h3 className="text-lg font-semibold mb-2">Company information</h3>
                 <label className="block mb-4">
-                  Company:
+                  Företagsnamn
                   <input
                     type="text"
                     name="company"
                     value={formData.company!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
                 <label className="block mb-4">
-                  Street Line 1:
+                  Address (Line 1)
                   <input
                     type="text"
                     name="streetLine1"
                     value={formData.streetLine1!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
                 <label className="block mb-4">
-                  City:
-                  <input
-                    type="text"
-                    name="city"
-                    value={formData.city!}
-                    onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
-                  />
-                </label>
-
-                <label className="block mb-4">
-                  Street Line 2:
+                  Address (Line 2)
                   <input
                     type="text"
                     name="streetLine2"
                     value={formData.streetLine2!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
                 <label className="block mb-4">
-                  Province:
+                  Stad
+                  <input
+                    type="text"
+                    name="city"
+                    value={formData.city!}
+                    onChange={handleChange}
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                  />
+                </label>
+
+                <label className="block mb-4">
+                  Province
                   <input
                     type="text"
                     name="province"
                     value={formData.province!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
                 <label className="block mb-4">
-                  Postal Code:
+                  Postnummer
                   <input
                     type="text"
                     name="postalCode"
                     value={formData.postalCode!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
                 <label className="block mb-4">
-                  Country Code:
+                  Landskod
                   <input
                     type="text"
                     name="countryCode"
                     value={formData.countryCode!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
                 <br />
                 <h3 className="text-lg font-semibold mb-2">Contact information</h3>
                 <label className="block mb-4">
-                  Full Name:
+                  Namn
                   <input
                     type="text"
                     name="fullName"
                     value={formData.fullName!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
                 <label className="block mb-4">
-                  Phone Number:
+                  Telefonnummer
                   <input
                     type="text"
                     name="phoneNumber"
                     value={formData.phoneNumber!}
                     onChange={handleChange}
-                    className="form-input mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
+                    className="form-input mt-1 px-2 py-1 lock w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-300 focus:border-blue-300"
                   />
                 </label>
 
