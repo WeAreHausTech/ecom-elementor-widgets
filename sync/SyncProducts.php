fe619d75e65540bb44c5bbfb5f81e4809798ee96
Johanna Bertils <johanna@haus.se>
2023-09-26 13:39:26 +0200

    Import products from vendure and update, create, delete in wp

--- sync/SyncProducts.php
+++ sync/SyncProducts.php
@@ -4,31 +4,30 @@
  wp sync-products sync
 */
 
-
 class syncProducts extends WP_CLI_Command
 {
-
     private $created = 0;
-
     private $updated = 0;
+    private $total = 0;
+    private $deleted = 0;
 
     public function sync()
     {
-        $products = $this->getAllProducts();
+        $vendureProducts = $this->getAllProductsFromVendure();
 
-        if (!$products) {
+        $wpProducts = $this->getAllProductsFromWp();
+
+        if (!$vendureProducts) {
             WP_CLI::error('No products found');
             return;
         }
 
-        foreach ($products as $product) {
-            $this->syncProduct($product);
-        }
+        $this->syncProducts($vendureProducts, $wpProducts);
 
-        WP_CLI::success('Created: ' . $this->created . ', updated: ' . $this->updated);
+        WP_CLI::success('Total: ' . $this->total . ', updated: ' . $this->updated . ' deleted: ' . $this->deleted . ', created: ' . $this->created);
     }
 
-    public function getAllProducts()
+    public function getAllProductsFromVendure()
     {
         $products = (new \Haus\Queries\Product)->get();
 
@@ -38,68 +37,130 @@ class syncProducts extends WP_CLI_Command
         return $products['data']['products']['items'];
     }
 
-    public function syncProduct($product)
+    public function getAllProductsFromWp()
     {
-        $id = $product['id'];
-
         global $wpdb;
-        $exists = $wpdb->get_results("SELECT post_id FROM $wpdb->postmeta WHERE meta_key = 'vendure_id' AND meta_value LIKE '%$id%'", ARRAY_A);
 
-        $existingId = empty($exists) ? false : $exists[0];
+        $table_name_posts = $wpdb->prefix . 'posts';
+        $table_name_postmeta = $wpdb->prefix . 'postmeta';
+        $post_type = 'produkter';
 
-        if ($existingId) {
-            $this->updateProduct($existingId, $product);
-        } else {
-            $this->createProduct($product);
+        $query = $wpdb->prepare(
+            "SELECT p.ID, p.post_title, pm.meta_key, pm.meta_value
+             FROM $table_name_posts p 
+             LEFT JOIN $table_name_postmeta pm ON p.ID = pm.post_id
+             WHERE post_type = %s",
+            $post_type
+        );
+
+        $products = $wpdb->get_results($query, ARRAY_A);
+        $productsData = [];
+
+        foreach ($products as $product) {
+            $productId = $product['ID'];
+
+            if (!isset($productsData[$productId])) {
+                $productsData[$productId] = array(
+                    'id' => $productId,
+                    'post_title' => $product['post_title'],
+                );
+            }
+
+            $productsData[$productId][$product['meta_key']] = $product['meta_value'];
+        }
+
+        return $productsData;
+    }
+
+    public function syncProducts($vendureProducts, $wpProducts)
+    {
+        foreach ($wpProducts as $wpProduct) {
+            $foundInVendure = false;
+
+            if (!isset($wpProduct['vendure_id'])) {
+                $this->deleteProduct($wpProduct['id']);
+                continue;
+            }
+
+            foreach ($vendureProducts as $vendureProduct) {
+                if ($wpProduct['vendure_id'] === $vendureProduct['id']) {
+                    $foundInVendure = true;
+                    $this->updateProduct($wpProduct, $vendureProduct);
+                    $this->total++;
+                    break;
+                }
+            }
+
+            if (!$foundInVendure) {
+                $this->deleteProduct($wpProduct['id']);
+            }
+        }
+
+        foreach ($vendureProducts as $vendureProduct) {
+            $foundInWordPress = false;
+
+            foreach ($wpProducts as $wpProduct) {
+                if (!isset($wpProduct['vendure_id'])) {
+                    return;
+                }
+
+                if ($vendureProduct['id'] === $wpProduct['vendure_id']) {
+                    $foundInWordPress = true;
+                    break;
+                }
+            }
+
+            if (!$foundInWordPress) {
+                $this->createProduct($vendureProduct);
+            }
         }
     }
 
-    public function updateProduct($postId, $product)
+    public function deleteProduct($postId)
     {
         global $wpdb;
-
-        $titleInWp = $wpdb->get_var(
-            $wpdb->prepare(
-                "SELECT post_title
-                FROM $wpdb->posts
-                WHERE ID = %d",
-                $postId
-            )
+        $wpdb->query(
+            $wpdb->prepare("DELETE FROM $wpdb->posts WHERE ID = %d", $postId)
         );
 
-        $slugInWp = $wpdb->get_var(
-            $wpdb->prepare(
-                "SELECT meta_value
-                FROM $wpdb->postmeta
-                WHERE post_id = %d AND meta_key = 'vendure_slug'",
-                $postId
-            )
+        $wpdb->query(
+            $wpdb->prepare("DELETE FROM $wpdb->postmeta WHERE post_id = %d", $postId)
         );
 
-        $updateName = $titleInWp !== $product['name'];
-        $updateSlug = $slugInWp !== $product['slug'];
+        $this->deleted++;
+    }
 
+    public function updateProduct($wpProduct, $vendureProduct)
+    {
+
+        $updateName = $wpProduct['post_title'] !== $vendureProduct['name'];
+        $updateSlug = $wpProduct['vendure_slug'] !== $vendureProduct['slug'];
+
+        global $wpdb;
         if ($updateName) {
-
-            // funkar inte ???
-
-            $new = $product['name']; 
-            wp_update_post (array(
-                'ID'           => $postId,
-                'post_title'   => $new, // new title
-            ));            
+            $wpdb->update(
+                $wpdb->posts,
+                array('post_title' => $vendureProduct['name']),
+                array('ID' => $wpProduct['id']),
+                array('%s'),
+                array('%d')
+            );
         }
 
         if ($updateSlug) {
-            update_post_meta($postId, 'vendure_slug', $product['slug']);
+            $wpdb->query(
+                $wpdb->prepare(
+                    "UPDATE $wpdb->postmeta SET meta_value = %s WHERE post_id = %d AND meta_key = %s",
+                    $vendureProduct['slug'],
+                    $wpProduct['id'],
+                    'vendure_slug'
+                )
+            );
         }
 
         if ($updateName || $updateSlug) {
-      
-
             $this->updated++;
         }
-        // update_post_meta($postId, 'vendure_updatedAt', $product['updatedAt']);
     }
 
     public function createProduct($product)
@@ -111,7 +172,6 @@ class syncProducts extends WP_CLI_Command
             'meta_input' => [
                 'vendure_id' => $product['id'],
                 'vendure_slug' => $product['slug'],
-                // 'vendure_updatedAt' => $product['updatedAt'],
             ]
         ];
 
@@ -120,6 +180,5 @@ class syncProducts extends WP_CLI_Command
         $this->created++;
     }
 }
-
 
 WP_CLI::add_command('sync-products', 'syncProducts');
