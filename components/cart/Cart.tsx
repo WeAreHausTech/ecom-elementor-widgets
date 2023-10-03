3f74e4a2cead7c8fd6e30f5dcdff703fed737eb1
Johanna Bertils <johanna@haus.se>
2023-10-03 12:58:37 +0200

    fix elementor styling overrides

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -2,10 +2,9 @@ import { CartOrderLines } from '../cart-order-lines/CartOrderLines'
 import { CartPrice } from '../cart-price/CartPrice'
 import { Button } from '../button/Button'
 import { CartTotals } from '@haus-tech/ecom-components'
-import { useNavigate } from '@tanstack/react-router'
+
 
 export const Cart = () => {
-  const navigate = useNavigate()
   const order = {
     totalWithTax: 1,
     subTotal: 2,
@@ -34,14 +33,24 @@ export const Cart = () => {
           </CartTotals>
         </div>
 
-        <Button color="blue" type="submit" className="cart-page-button"  onClick={() => navigate({ to: '/kassa' })}>
+        <Button
+          color="blue"
+          type="submit"
+          className="cart-page-button"
+          onClick={() => (window.location.href = '/kassa')}
+        >
           Gå till kassan
         </Button>
       </div>
 
       <div className="cart-summary">
         <CartPrice className="cart-page-price" subTotal tax shipping totalWithTax order={order} />
-        <Button color="blue" type="submit" className="mt-4 cart-page-button" onClick={() => navigate({ to: '/kassa' })}>
+        <Button
+          color="blue"
+          type="submit"
+          className="mt-4 cart-page-button"
+          onClick={() => (window.location.href = '/kassa')}
+        >
           Gå till kassan
         </Button>
       </div>
