6d9950f1f9f3127549192b11c6a8f024da445123
Mathias Saxenstrand <mathias@haus.se>
2023-10-04 14:47:25 +0200

    Product detail category, sku and change quantity

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,17 +1,25 @@
 import classNames from 'classnames'
-import { createElement } from 'react'
+import { createElement, forwardRef } from 'react'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode
+  children?: React.ReactNode
   color?: 'default' | 'blue' | 'white'
   variant?: 'default' | 'outline'
   rounded?: 'default' | 'full'
 }
 
-export const Button = ({ children, color, variant, rounded, className, ...props }: ButtonProps) => {
-  return createElement(
-    'button',
-    Object.assign({ className: classNames('Button', className, color, variant, rounded) }, props),
-    children,
-  )
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, color, variant, rounded, className, ...props }, ref) => {
+    return createElement(
+      'button',
+      {
+        ...Object.assign(
+          { className: classNames('Button', className, color, variant, rounded) },
+          props,
+        ),
+        ref,
+      },
+      children,
+    )
+  },
+)
