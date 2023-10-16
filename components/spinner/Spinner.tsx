06c7487b94d8e44508555606f9356714d7de7fa4
Mathias Saxenstrand <mathias@haus.se>
2023-10-16 14:57:51 +0200

    Cleanup widgets repo... (#22)
    * Cleanup widgets repo...
    
    * @haus-tech/ecom-components@0.0.56
    
    * remove unused styles

--- components/spinner/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-export const Spinner = () => {
-  return (
-    <div className="flex gap-1">
-      <div className="w-3 h-3 rounded-full animate-pulse bg-black"></div>
-      <div className="w-3 h-3 rounded-full animate-pulse bg-black"></div>
-      <div className="w-3 h-3 rounded-full animate-pulse bg-black"></div>
-    </div>
-  )
-}
