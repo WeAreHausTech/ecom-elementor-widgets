cc1482aa5f691354d446a43be6eb012e31c587ce
Mathias Saxenstrand <mathias@haus.se>
2023-12-13 13:58:53 +0100

    Terser

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import terser from '@rollup/plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': {}
   },
-  plugins: [react()],
+  plugins: [react(), terser()],
   build: {
     outDir: 'dist',
     manifest: true,
+    minify: 'terser',
     lib: {
       entry: 'components/index.tsx',
       name: 'ecom-components',
@@ -19,7 +21,10 @@ export default defineConfig({
     rollupOptions: {
       // external: ['react', 'react-dom'],
       output: {
+        compact: true,
         inlineDynamicImports: false,
+        format: 'es',
+        minifyInternalExports: true,
         // manualChunks: { 'ecom': ['@haus-tech/ecom-components'] },
         entryFileNames: '[name]-[hash].js',   // currently does not work for the legacy bundle
         assetFileNames: '[name]-[hash].[ext]', // currently does not work for images
