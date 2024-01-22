3b2bb125087fa62bb5dabb97366a8717b5463b66
Mathias Saxenstrand <mathias@haus.se>
2024-01-22 12:18:39 +0100

    Account dropdown widget (#69)

--- src/Widgets/AccountDropdown.php
+++ src/Widgets/AccountDropdown.php
@@ -35,7 +35,69 @@ class AccountDropdown extends Widget_Base
 
     protected function register_controls()
     {
-        
+        $this->start_controls_section(
+            'section_general',
+            [
+                'label' => __('General settings', 'haus-ecom-widgets'),
+            ]
+        );
+
+        $this->add_control(
+            'use_login_modal',
+            [
+                'label' => __('Use login modal', 'haus-ecom-widgets'),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'default' => '0',
+                'options' => [
+                    '0' => __('No', 'haus-ecom-widgets'),
+                    '1' => __('Yes', 'haus-ecom-widgets'),
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'on_logout_redirect',
+            [
+                'label' => __('Redirect on logout', 'haus-ecom-widgets'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+            ]
+        );
+
+        $repeater = new \Elementor\Repeater();
+
+        $repeater->add_control(
+            'href',
+            [
+                'label' => __('Href:', 'haus-ecom-widgets'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'label_block' => true,
+                'default' => '',
+            ]
+        );
+
+        $repeater->add_control(
+            'label',
+            [
+                'label' => __('Label:', 'haus-ecom-widgets'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'label_block' => true,
+                'default' => '',
+            ]
+        );
+
+        $this->add_control(
+			'dropdown-items',
+			[
+				'label' => __( 'Menu items:', 'haus-ecom-widgets' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => $repeater->get_controls(),
+				'default' => [],
+				'title_field' => '{{{ label }}}',
+                'prevent_empty' => false,
+			]
+		);
+
+        $this->end_controls_section();
     }
 
     protected function render()
@@ -56,9 +118,9 @@ class AccountDropdown extends Widget_Base
             data-vendure-api-url="<?= VENDURE_API_URL ?>"
             data-widget-type="account-dropdown"
             data-use-login-modal="<?= $settings['use_login_modal'] ?>"
-            data-icon-component="<?= $settings['icon_component'] ?>"
             data-on-logout-redirect="<?= $settings['on_logout_redirect'] ?>"
-        >
+            data-dropdown-items="<?= htmlspecialchars(json_encode($settings['dropdown-items']), ENT_QUOTES, 'UTF-8'); ?>" 
+        >   
         </div>
         <?php
     }
