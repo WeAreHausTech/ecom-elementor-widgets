449c440b86632c59dd41138707d6ea624cbda0ad
Johanna Bertils <johanna@haus.se>
2024-04-26 14:29:15 +0200

    fix delete not working with wp-rocket

--- src/SyncData/Classes/Taxonomies.php
+++ src/SyncData/Classes/Taxonomies.php
@@ -65,11 +65,10 @@ class Taxonomies
     {
         foreach ($vendureTerms as $vendureId => $vendureTerm) {
             foreach ($wpTerms as $wpId => $wpTerm) {
-                $decodedWpName = html_entity_decode($wpTerm['name'] ?? null);
-                if ($wpId === $vendureId && $decodedWpName !== $vendureTerm['name']) {
-                    $this->deleteTerm($wpTerm['term_id'] ?? null, $taxonomy);
+                if ($wpTerm['name'] === null || ($wpId === $vendureId &&  html_entity_decode($wpTerm['name']) !== $vendureTerm['name'])) {
                     $this->deleteTranslation($taxonomy, $wpTerm);
-                    WpHelper::log(['Deleted taxonomy missmatch', $taxonomy, $vendureTerm['name'], $decodedWpName]);
+                    $this->deleteTerm($wpTerm['term_id'] ?? null, $taxonomy);
+                    WpHelper::log(['Deleted taxonomy missmatch', $taxonomy, $vendureTerm['name']]);
                 }
             }
         }
@@ -82,9 +81,8 @@ class Taxonomies
         $delete = array_diff_key($wpTerms, $vendureTerms);
 
         array_walk($delete, function ($term) use ($taxonomy) {
-            $this->deleteTerm($term['term_id'], $taxonomy);
-
             $this->deleteTranslation($taxonomy, $term);
+            $this->deleteTerm($term['term_id'], $taxonomy);
         });
 
         //Exists in Vendure, not in WP
@@ -247,11 +245,26 @@ class Taxonomies
 
     public function deleteTerm($id, $taxonomy)
     {
-        //TODO use this SQL to make it safer:
-        //TODO 'DELETE FROM termmeta WHERE term_id NOT IN (SELECT term_id FROM terms)'
-        delete_term_meta($id, 'vendure_collection_id');
-        delete_term_meta($id, 'vendure_term_id');
-        wp_delete_term($id, $taxonomy);
+        global $wpdb;
+
+        $wpdb->query(
+            $wpdb->prepare(
+                "DELETE FROM $wpdb->termmeta WHERE term_id = %d",
+                $id
+            )
+        );
+        
+        $wpdb->delete(
+            $wpdb->term_taxonomy,
+            array('term_id' => $id),
+            array('%d')
+        );
+
+        $wpdb->delete(
+            $wpdb->terms,
+            array('term_id' => $id),
+            array('%d')
+        );
 
         WpHelper::log(['Deleting taxonomy', $taxonomy, $id]);
 
