6bf0ff42fa0a99239d3fab4b90248f4ece87495b
Johanna Bertils <johanna@haus.se>
2024-05-24 14:35:30 +0200

    select root collection in sync

--- src/Queries/Collection.php
+++ src/Queries/Collection.php
@@ -3,34 +3,49 @@ namespace WeAreHausTech\Queries;
 
 class Collection extends BaseQuery
 {
-    public function get($lang, $skip, $take)
+    public function get($rootCollection, $lang)
     {
 
-        $config = require(HAUS_ECOM_PLUGIN_PATH . '/config.php');
+        $config = require (HAUS_ECOM_PLUGIN_PATH . '/config.php');
 
         $customFields = $config['productSync']['collections']['customFieldsQuery'] ?? '';
 
-
-        $options = "(options: {
-            take: $take,
-            skip: $skip
+        $options = "(input: {
+            take: 9999,
+            collectionId: $rootCollection
         })";
 
-        $this->query = 
-           "query{
-                collections $options{
-                    totalItems
-                    items {
-                    id
-                    name
-                    slug
-                    parentId
-                    updatedAt
+        $options = "(input: {
+            take: 9999" . (!empty($rootCollection) ? ",
+            collectionId: $rootCollection" : '') . "
+        })";
+
+
+        $this->query =
+            "query{
+                search $options{
+                    collections {
+                        collection {
+                          id
+                        name
+                        slug
+                        parentId
+                        updatedAt
                     $customFields
+                        }
                     }
                 }
             }";
 
-        return $this->fetch($lang);
+        $collections = $this->fetch($lang);
+
+        $filteredCollections = array_filter($collections['data']['search']['collections'], function ($collectionWrapper) use ($rootCollection) {
+            if (empty ($rootCollection)) {
+                return true;
+            }
+
+            return $collectionWrapper['collection']['id'] !== $rootCollection;
+        });
+        return $filteredCollections;
     }
 }
