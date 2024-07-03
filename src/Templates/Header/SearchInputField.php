56dd46df723568f54c806f15f020f06386deaf75
Maria Pettersson <mariateresepettersson@gmail.com>
2024-07-03 14:40:51 +0200

    New html object import

---
+++ src/Templates/Header/SearchInputField.php
@@ -0,0 +1,15 @@
+<div class="search-field-wrapper" id="search-field-widget">
+  <?php
+  $widget_id = 'ecom_' . uniqid();
+  echo '<div 
+      id="' . $widget_id . '" 
+      data-open-on-button="false"
+      class="ecom-components-root" 
+      data-vendure-token="' . VENDURE_TOKEN . '"
+      data-vendure-api-url="' . VENDURE_API_URL . '"
+      data-widget-type="search-dialogue"
+      data-placeholder="' . $data['search_placeholder'] . '"
+      >
+  </div>';
+  ?>
+</div>
