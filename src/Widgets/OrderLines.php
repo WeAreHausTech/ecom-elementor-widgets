94a56f2485adb86f84a71e80d211bb354a906abc
Emmy Dieden <emmy@Emmys-MacBook-Pro-20.local>
2024-08-29 13:20:39 +0200

    Build CartPrice.php

--- src/Widgets/OrderLines.php
+++ src/Widgets/OrderLines.php
@@ -30,7 +30,7 @@ class OrderLines extends Widget_Base
     
     public function get_keywords()
     {
-        return ['Ecommerce', 'orderlines'];
+        return ['Ecommerce', 'orderlines', 'cart'];
     }
     
     //Osäker på om jag ska registrera kontroller
@@ -53,14 +53,7 @@ class OrderLines extends Widget_Base
         <div 
             id="<?= $widgetId ?>"
             class="ecom-components-root" 
-            data-vendure-token="<?= VENDURE_TOKEN ?>"
-            data-vendure-api-url="<?= VENDURE_API_URL ?>"
-            data-show-subtotal="<?= $settings['show_subTotal']  ?>"
-            data-show-tax="<?= $settings['show_tax']  ?>"
-            data-show-shipping="<?=  $settings['show_shipping'] ?>"
-            data-show-total="<?=  $settings['show_total'] ?>"
-            data-custom-message="<?= $settings['custom-message']?>"
-            data-widget-type="cart">
+            data-widget-type="orderlines">
         </div>
         <?php
     }
