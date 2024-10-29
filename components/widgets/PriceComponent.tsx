b4e6665ea03f815c3efa7bf16855a231e10aaaf2
Mathias Saxenstrand <mathias@haus.se>
2024-10-29 14:47:16 +0100

    Update @haus-tech/ecom-components to version 1.3.5 Refactor PriceComponent to handle product details and show skeleton loader

--- components/widgets/PriceComponent.tsx
+++ components/widgets/PriceComponent.tsx
@@ -1,1 +1,29 @@
-export { PriceComponent as default } from '@haus-tech/ecom-components/store-components'
+import { Product, RequireAtLeastOne, SearchResult } from '@haus-tech/ecom-components'
+import { useProductDetail } from '@haus-tech/ecom-components/hooks'
+import { PriceComponent as EcomPriceComponent } from '@haus-tech/ecom-components/store-components'
+import { ComponentProps } from 'react'
+
+type ProductInputVariable = RequireAtLeastOne<{
+  productId: string
+  productSlug: string
+  product?: Pick<Product | SearchResult, 'currencyCode' | 'price' | 'priceWithTax'>
+}>
+
+type Props = ComponentProps<typeof EcomPriceComponent> & ProductInputVariable
+
+const PriceComponent = ({
+  productId,
+  productSlug,
+  showSkeletonLoader,
+  product: incomingProduct,
+  ...rest
+}: Props) => {
+  const { data: product, isLoading } = useProductDetail(
+    productId ? { id: productId } : { slug: productSlug! },
+  )
+
+  if (!showSkeletonLoader && isLoading && !product) return null
+  return <EcomPriceComponent product={product || incomingProduct} {...rest} />
+}
+
+export default PriceComponent
